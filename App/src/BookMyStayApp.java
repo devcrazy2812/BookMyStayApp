313da9d548371cdc21d0923e8b425253bf2ec162
devcrazy2812 <devcrazy2812@gmail.com>
2026-04-08 14:23:06 +0530

    ABHAY GOYAL ADDED UC2-Room-Types

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,7 +1,78 @@
-public class BookMyStayApp {
-    static void main() {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System Initialized Successfully.");
+// Version 2.1
+
+// Abstract Room Class
+abstract class Room {
+
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
     }
 
+    // Method to display room details
+    public void displayDetails(String roomType, int availability) {
+        System.out.println(roomType + ":");
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+        System.out.println("Available: " + availability);
+        System.out.println();
+    }
+}
+
+
+// Single Room Class
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+
+// Double Room Class
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+
+// Suite Room Class
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+
+// Main Application Class
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        // Create room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Static availability variables
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        // Display room details
+        singleRoom.displayDetails("Single Room", singleAvailability);
+        doubleRoom.displayDetails("Double Room", doubleAvailability);
+        suiteRoom.displayDetails("Suite Room", suiteAvailability);
+    }
 }
